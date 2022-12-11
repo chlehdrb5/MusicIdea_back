38d25cc1c6faf47a04a2c979cc478a49c8dc1e37
chlehdrb5 <chlehdrb0513@gmail.com>
2022-12-11 15:51:29 +0900

    post : pagination, ordering

--- post/views.py
+++ post/views.py
@@ -1,16 +1,27 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.filters import OrderingFilter
 
 from post.models import Post
 from post.serializer import PostSerializer
 
 
+class PostPagination(PageNumberPagination):
+    page_size = 9
+
+
 class PostViewSet(ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    pagination_class = PostPagination
+
+    filter_backends = [OrderingFilter]
+    ordering_fields = []
+    ordering = ['-id']
 
     lookup_url_kwarg = 'post_id'
 
